eac2d830aec4474a6f9df58459f9d39359bdb071
Anganor <anganor@gmail.com>
2019-08-02 15:39:43 +0200

    first commit

---
+++ src/com/kodilla/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+
+public class Calculator {
+
+    void addition(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    void subtraction(int a, int b) {
+        System.out.println(a - b);
+    }
+}
---
+++ src/com/kodilla/Main.java
@@ -0,0 +1,14 @@
+package com.kodilla;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int a = 5;
+	    int b = 3;
+
+	    Calculator calc = new Calculator();
+	    calc.addition(4, 6);
+	    calc.subtraction(a, b);
+
+    }
+}
